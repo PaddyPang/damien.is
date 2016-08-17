61f545bcd94e030ed5d534c4ba277497c843ce78
TheKoon <damien@damien.is>
2016-08-17 20:03:33 +0200

    Add 404 error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('body-class', 'alt-5')
+
+@section('body')
+    @include('layouts.header')
+
+    <main class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-9">
+                    <h1>This page does not exist.</h1>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    @include('layouts.footer')
+@endsection
