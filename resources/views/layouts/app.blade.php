0976b9c49ff275b203fbcd349244909a333c84b7
TheKoon <damien@damien.is>
2016-08-17 15:20:01 +0200

    Add cache buster for favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,8 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <meta name="apple-mobile-web-app-title" content="Damien Criado">
 
-    <link rel="apple-touch-icon" href="/images/apple-touch-icon.png"/>
-    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico"/>
+    <link rel="apple-touch-icon" href="{{ Bust::url('/images/apple-touch-icon.png') }}'"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ Bust::url('/images/favicon.ico') }}'"/>
     <!--[if IE]>
     <script src="/bower_components/html5shiv/dist/html5shiv.min.js"></script>
     <![endif]-->
