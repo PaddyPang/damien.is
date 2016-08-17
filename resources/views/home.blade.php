07564296eb0a162d1c80fd3bb000d66216237872
TheKoon <damien@damien.is>
2016-08-17 14:34:53 +0200

    Update style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,11 @@
 @extends('layouts.app')
 
-@section('body-class', 'blue-pink')
+@section('body-class', 'alt-1')
 
 @section('body')
-    <header class="header">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">
-                        <span data-shuffle>damien.is</span><span class="alt" data-shuffle>/coding</span>
-                    </a>
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
+    @include('layouts.header')
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Project</a></li>
-                        <li><a href="#">Blog</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-    </header>
-
-    <main>
+    <main class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-12 col-sm-9">
@@ -42,36 +18,5 @@
         </div>
     </main>
 
-    <footer class="footer">
-        <div class="container-fluid">
-            <hr>
-            <div class="row">
-                <div class="col-xs-12 col-sm-3">
-                    <ul class="list-unstyled">
-                        <li>Damien Criado</li>
-                        <li><a href="mailto:hello@damien.is">hello@damien.is</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-12 col-sm-3">
-                    <ul class="list-unstyled">
-                        <li><a href="https://github.com/damiencriado" target="_blank">GitHub</a></li>
-                        <li><a href="http://stackshare.io/damiencriado" target="_blank">StackShare</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-12 col-sm-3">
-                    <ul class="list-unstyled">
-                        <li><a href="https://twitter.com/damiencriado" target="_blank">Twitter</a></li>
-                        <li><a href="https://linkedin.com/in/damiencriado" target="_blank">LinkedIn</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-12 col-sm-3">
-                    <ul class="list-unstyled">
-                        <li><a href="http://www.yka.ch/" target="_blank">Lead Developer at<br>YKA SAS</a></li>
-                    </ul>
-                </div>
-            </div><!-- .row -->
-            <div class="humans">made by <a href="/humans.txt" target="_blank">human</a> / powered by
-                <a href="https://laravel.com/" target="_blank">laravel</a></div>
-        </div>
-    </footer>
+    @include('layouts.footer')
 @endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-unstyled">
+                    <li>Damien Criado</li>
+                    <li><a href="mailto:hello@damien.is">hello@damien.is</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-unstyled">
+                    <li><a href="https://github.com/damiencriado" target="_blank">GitHub</a></li>
+                    <li><a href="http://stackshare.io/damiencriado" target="_blank">StackShare</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-unstyled">
+                    <li><a href="https://twitter.com/damiencriado" target="_blank">Twitter</a></li>
+                    <li><a href="https://linkedin.com/in/damiencriado" target="_blank">LinkedIn</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-3">
+                <ul class="list-unstyled">
+                    <li><a href="http://www.yka.ch/" target="_blank">Lead Developer at<br>YKA SAS</a></li>
+                </ul>
+            </div>
+        </div><!-- .row -->
+        <div class="humans">
+            made by <a href="/humans.txt" target="_blank">human</a> / powered by <a href="https://laravel.com/" target="_blank">laravel</a>
+        </div>
+        <div class="opensource">
+            <a href="https://github.com/damiencriado/damien.is" target="_blank">get this website source</a>
+        </div>
+    </div>
+</footer>
