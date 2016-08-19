b4109bce812a515997e5ca4c4ba1d0400151f513
TheKoon <damien@damien.is>
2016-08-20 01:26:56 +0200

    Typo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-12 col-sm-9">
-                    <h1>Building app, experimenting with new web technologies.</h1>
+                    <h1>Building apps, experimenting with new web technologies.</h1>
                 </div>
                 <div class="col-xs-12 col-sm-3">
                     <img class="damien" src="{{ Bust::url('/images/damien.jpg') }}">
